5fb2c53690e82a1e46d4e87659e3761cea68a6ab
Marcos <marcosscpp@gmail.com>
2025-04-17 11:22:57 -0300

    Updating Build Config Again...

--- src/@types/xml2js.d.ts
+++ src/@types/xml2js.d.ts
@@ -1,1 +1,51 @@
-declare module "xml2js";
+declare module "xml2js" {
+  export interface ParserOptions {
+    attrkey?: string;
+    charkey?: string;
+    explicitArray?: boolean;
+    normalize?: boolean;
+    normalizeTags?: boolean;
+    explicitRoot?: boolean;
+    emptyTag?: any;
+    explicitCharkey?: boolean;
+    trim?: boolean;
+    attrNameProcessors?: Array<(name: string) => string>;
+    attrValueProcessors?: Array<(value: string, name: string) => any>;
+    tagNameProcessors?: Array<(name: string) => string>;
+    valueProcessors?: Array<(value: string, name: string) => any>;
+    xmlns?: boolean;
+  }
+
+  export class Parser {
+    constructor(options?: ParserOptions);
+    parseString(
+      str: string,
+      callback?: (err: Error, result: any) => void
+    ): void;
+    parseStringPromise(str: string): Promise<any>;
+  }
+
+  export function parseString(
+    str: string,
+    callback?: (err: Error, result: any) => void
+  ): void;
+  export function parseString(
+    str: string,
+    options: ParserOptions,
+    callback?: (err: Error, result: any) => void
+  ): void;
+  export function parseStringPromise(
+    str: string,
+    options?: ParserOptions
+  ): Promise<any>;
+
+  export const defaults: {
+    parseString: ParserOptions;
+  };
+
+  export const processors: {
+    parseNumbers: (value: string) => number;
+    parseBooleans: (value: string) => boolean;
+    stripPrefix: (value: string) => string;
+  };
+}
